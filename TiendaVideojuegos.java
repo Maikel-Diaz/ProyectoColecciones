852e2a31eae8a49ca85d3873fc77a27ddfdacd4a
Michael <maikel_20@hotmail.es>
2018-01-24 10:51:50 +0100

    He creado dos nuevas clases, TiendaVideojuegos y Videojuegos

---
+++ TiendaVideojuegos.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class TiendaVideojuegos here.
+ * 
+ * @author (Maikel) 
+ * @version (a version number or a date)
+ */
+public class TiendaVideojuegos
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class TiendaVideojuegos
+     */
+    public TiendaVideojuegos()
+    {
+        // initialise instance variables
+    }
+}
---
+++ Videojuegos.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Videojuegos here.
+ * 
+ * @author (Maikel) 
+ * @version (a version number or a date)
+ */
+public class Videojuegos
+{
+    // instance variables - replace the example below with your own
+    
+    /**
+     * Constructor for objects of class Videojuegos
+     */
+    public Videojuegos()
+    {
+        // initialise instance variables
+        
+    }
+}
